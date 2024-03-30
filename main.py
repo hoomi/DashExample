bd7c374d6c09c1a448696e5550b27eeaec6c9e57
hoomi <hoomi2010@gmail.com>
2024-03-30 14:14:33 -0700

    Add the code for deploying to Google Cloud correctly

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from dash import Dash, html, dcc, callback, Output, Input
+
 import plotly.express as px
 import pandas as pd
 import os
+import datetime
+import pandas as pd
+
+
+dash = Dash(__name__)
+app=dash.server
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
-
-app = Dash(__name__)
-server=app.server
-
-app.layout = html.Div([
+dash.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
+
 
 @callback(
     Output('graph-content', 'figure'),
@@ -22,5 +26,7 @@ def update_graph(value):
     dff = df[df.country==value]
     return px.line(dff, x='year', y='pop')
 
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    dash.run_server(host="127.0.0.1", port=int(os.environ.get("PORT", 8080)))
